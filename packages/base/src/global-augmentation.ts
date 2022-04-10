37d24e36b0c1e4818d5b181c578014760a362bf2
Peter Krol <peter@peterkrol.com>
2022-04-10 18:31:31 -0400

    refactor: delete unnecessary barrels

--- packages/base/src/global-augmentation.ts
+++
@@ -1,5 +0,0 @@
-export {} from "./collection/Array.js";
-export {} from "./collection/Iterable.js";
-export {} from "./data/number.js";
-export {} from "./data/string.js";
-export {} from "./prelude/builtin.js";
