1e66dcce977f0371a21751262816d092afc7afb9
0x706b <peter@peterkrol.com>
2022-06-18 14:17:02 -0400

    chore: delete demo from express package

--- packages/express/src/demo.ts
+++
@@ -1,7 +0,0 @@
-import * as Express from "./index.js";
-
-const route = Express.get("/", (_, res) => IO.succeed(res.send("Hello, world!")));
-
-const app = Express.LiveExpress("localhost", 3000);
-
-(route.provideSomeLayer(app) > IO.never).unsafeRunAsyncWith((exit) => console.log(exit));
