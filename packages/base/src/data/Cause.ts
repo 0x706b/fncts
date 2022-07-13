3e7c66a45c753127f4ef865097c91184458528a1
0x706b <peter@peterkrol.com>
2022-07-13 17:28:10 -0400

    fix: Cause and Z circular dependency

--- packages/base/src/data/Cause.ts
+++ packages/base/src/data/Cause.ts
@@ -2,3 +2,11 @@
 export * from "./Cause/api.js";
 export * from "./Cause/definition.js";
 // codegen:end
+
+// codegen:start { preset: barrel, include: Cause/api/*.ts }
+export * from "./Cause/api/fold.js";
+export * from "./Cause/api/isEmpty.js";
+export * from "./Cause/api/linearize.js";
+export * from "./Cause/api/prettyPrint.js";
+export * from "./Cause/api/unified.js";
+// codegen:end
