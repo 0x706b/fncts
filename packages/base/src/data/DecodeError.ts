5607df5d076b57c929bfe9cbf703775e3d710955
0x706b <peter@peterkrol.com>
2022-08-09 21:53:56 -0400

    chore: update dependenices

--- packages/base/src/data/DecodeError.ts
+++ packages/base/src/data/DecodeError.ts
@@ -1,3 +1,4 @@
 // codegen:start { preset: barrel, include: ./DecodeError/*.ts }
 export * from "./DecodeError/definition.js";
+export * from "./DecodeError/instances.js";
 // codegen:end
--- packages/node/src/index.ts
+++ packages/node/src/index.ts
@@ -1,3 +1,4 @@
 // codegen:start { preset: type-barrel, include: ./*.ts }
 export type {} from "./fs.js";
+export type {} from "./global.js";
 // codegen:end
