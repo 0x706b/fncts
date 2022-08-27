9ed98befff3c97cca6c379054c7a2b7f57ea626b
0x706b <peter@peterkrol.com>
2022-08-27 14:17:24 -0400

    feat: add tag transformer

--- packages/base/src/data/Tag/constructors.ts
+++ packages/base/src/data/Tag/constructors.ts
@@ -1,6 +1,9 @@
+import { v4 } from "uuid";
+
 /**
  * @tsplus static fncts.TagOps __call
+ * @fncts tag
  */
-export function makeTag<T>(id: string): Tag<T> {
+export function makeTag<T>(id: string = v4()): Tag<T> {
   return new Tag(id);
 }
---
+++ packages/transformers/src/tag.ts
@@ -0,0 +1,29 @@
+import * as ts from "typescript";
+import { v4 } from "uuid";
+
+export default function transformTag(program: ts.Program, _config: {}): ts.TransformerFactory<ts.SourceFile> {
+  const checker = program.getTypeChecker();
+  return (context) => (sourceFile) => ts.visitEachChild(sourceFile, function visitor (node): ts.Node {
+    if (ts.isCallExpression(node)) {
+      const originalNode = ts.getOriginalNode(node);
+      const nodeLinks    = checker.getNodeLinks(originalNode);
+      if (nodeLinks.tsPlusCallExtension) {
+        const callExtension = nodeLinks.tsPlusCallExtension;
+        // @ts-expect-error
+        if (callExtension.patched.target?.tsPlusDeclaration) {
+          // @ts-expect-error
+          const declaration = callExtension.patched.target.tsPlusDeclaration as ts.Declaration;
+          const tags        = ts.getJSDocTags(declaration);
+          if (tags && tags.find((tag) => tag.tagName.escapedText === "fncts" && typeof tag.comment === "string" && tag.comment === "tag")) {
+            const update = context.factory.updateCallExpression(node, node.expression, node.typeArguments, [context.factory.createStringLiteral(v4())]);
+            // @ts-expect-error
+            update.parent = node.parent;
+            return update;
+          }
+        }
+      }
+    }
+    return ts.visitEachChild(node, visitor, context);
+  }, context);
+}
+
