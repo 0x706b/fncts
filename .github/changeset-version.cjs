0875ef6f3957d969d24605ecab2abde7892a5163
Peter Krol <peter@peterkrol.com>
2023-02-12 18:20:11 -0500

    chore: update dependencies

--- .github/changeset-version.cjs
+++ .github/changeset-version.cjs
@@ -1,4 +1,4 @@
 const { execSync } = require("node:child_process");
 
 execSync("npx changeset version");
-execSync("YARN_ENABLE_IMMUTABLE_INSTALLS=false node .yarn/releases/yarn-4.0.0-rc.14.cjs");
+execSync("YARN_ENABLE_IMMUTABLE_INSTALLS=false node .yarn/releases/yarn-4.0.0-rc.39.cjs");
