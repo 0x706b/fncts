65432a2bc30c39d1d8bc77552a54ea16b7d9bb18
Peter Krol <peter@peterkrol.com>
2023-04-23 12:56:54 -0400

    chore: update tsplus

--- packages/pattern/src/index.ts
+++ packages/pattern/src/index.ts
@@ -1,8 +1,14 @@
+import type { unset } from "ts-pattern/dist/internals/symbols";
+import type { Match } from "ts-pattern/dist/types/Match";
+
 import { match as _match } from "ts-pattern";
 
 /**
  * @tsplus getter global match
  */
-export const match = _match;
+export const match: {
+  <input, output = unset>(value: input): Match<input, output>;
+  <input extends [any, ...any], output = unset>(value: input): Match<input, output>;
+} = _match;
 
 export { isMatching, P, Pattern } from "ts-pattern";
