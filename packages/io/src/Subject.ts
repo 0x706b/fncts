158e8722ca9275a8f55def66912c7c134bf037d9
Peter Krol <peter@peterkrol.com>
2025-10-25 16:19:46 +0000

    chore: lint

--- packages/io/src/Subject.ts
+++ packages/io/src/Subject.ts
@@ -1,5 +1,7 @@
 // codegen:start { preset: barrel, include: ./Subject/*.ts }
-export * from "./Subject/api.js";
-export * from "./Subject/Atomic.js";
-export * from "./Subject/definition.js";
+export * from "./Subject/api.js"
+export * from "./Subject/Atomic.js"
+export * from "./Subject/DeferredRef.js"
+export * from "./Subject/definition.js"
+export * from "./Subject/Hold.js"
 // codegen:end
